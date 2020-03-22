284409f009b460e6fbde34e25f1368ab77a8ea2e
PongponZ <mr.pongpon.sinlapa.m.m@gmail.com>
2020-03-23 01:15:51 +0700

    add image gui

--- main.py
+++ main.py
@@ -7,36 +7,54 @@ from PIL import ImageTk,Image,ImageDraw,ImageFont
 osPath = os.path.dirname(__file__) 
 iconPath = osPath + "/assets/icons/virus2.ico"
 root = Tk()
-root.geometry("800x600")
-root.title("COVID-19 Information")
+root.geometry("980x700")
+root.title("COVID-19 Information || Dev by Pongpon Sinlapa")
+#root.configure(background='white')
 root.iconbitmap(iconPath)
 
 #set font
 font_path = osPath + "/fonts/Sarabun-Regular.ttf"
-font_type = ImageFont.truetype(font_path, 70)
+font_type = ImageFont.truetype(font_path, 30)
 
 
 #get data from api 
 #Github https://github.com/javieraviles/covidAPI?fbclid=IwAR2rYnlwkO4EcZ5kYKQxzj4QBbY13w1ND1rhv01gomWvpmaL7kcx-MpNVow
 res_global = requests.get("https://coronavirus-19-api.herokuapp.com/all")
+#res_countries = requests.get(" https://coronavirus-19-api.herokuapp.com/countries")
 data_global = res_global.json()
 
 #set data to image
 deaths = str(data_global['deaths'])
-
+recover = str(data_global['recovered'])
 
 
 #load image assets
 #load background RIP
-rip_path = Image.open(osPath + "/assets/img/rip.png")
-drawText = ImageDraw.Draw(rip_path)
-drawText.text(xy=(200, 400), text=deaths, fill=(0,0,0), font=font_type)
-ripImage_resize = rip_path.resize((150, 100), Image.ANTIALIAS)
-ripImage_file = ImageTk.PhotoImage(ripImage_resize) 
+deathsBG_path = Image.open(osPath + "/assets/img/deaths.png")
+drawText = ImageDraw.Draw(deathsBG_path).text(xy=(65, 60),
+                                         text=deaths,
+                                         font=font_type,
+                                         fill="#ffff")
+                                            
+deathsBG_resize = deathsBG_path.resize((250, 250), Image.ANTIALIAS)
+deathsBG_file = ImageTk.PhotoImage(deathsBG_resize) 
+deathsBG = Label(root, image=deathsBG_file)
 
-ripImage = Label(image = ripImage_file)
+#load background recover 
+recoverBG_path = Image.open(osPath + "/assets/img/recover.png")
+drawText = ImageDraw.Draw(recoverBG_path).text(xy=(65, 60),
+                                               text=recover,
+                                               font=font_type,
+                                               fill="#ffff")
+                                            
+recoverBG_resize = recoverBG_path.resize((250, 250), Image.ANTIALIAS)
+recoverBG_file = ImageTk.PhotoImage(recoverBG_resize) 
+recoverBG = Label(root, image=recoverBG_file)
 
-ripImage.grid(row=0, column = 0)
+
+#set Grid
+deathsBG.grid(row=0, column = 0)
+recoverBG.grid(row=1, column = 0)
 
 """
 #fetch data
