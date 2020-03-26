1eec906ca24a6fbf681e4ab9c8b8f7168b898976
PongponZ <mr.pongpon.sinlapa.m.m@gmail.com>
2020-03-26 14:02:45 +0700

    Update bg

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from PIL import ImageTk,Image,ImageDraw,ImageFont
 osPath = os.path.dirname(__file__) 
 iconPath = osPath + "/assets/icons/virus2.ico"
 root = Tk()
-root.geometry("980x700")
+root.geometry("975x570")
 root.title("COVID-19 Information || Dev by Pongpon Sinlapa")
 #root.configure(background='white')
 root.iconbitmap(iconPath)
 
 #set font
 font_path = osPath + "/fonts/Sarabun-Regular.ttf"
-font_type = ImageFont.truetype(font_path, 30)
+font_type = ImageFont.truetype(font_path, 35)
 
 
 #get data from api 
@@ -24,46 +24,29 @@ res_global = requests.get("https://coronavirus-19-api.herokuapp.com/all")
 data_global = res_global.json()
 
 #set data to image
+cases = str(data_global['cases'])
 deaths = str(data_global['deaths'])
 recover = str(data_global['recovered'])
 
 
 #load image assets
 #load background RIP
-deathsBG_path = Image.open(osPath + "/assets/img/deaths.png")
-drawText = ImageDraw.Draw(deathsBG_path).text(xy=(65, 60),
+bg_path = Image.open(osPath + "/assets/img/bg.png")
+drawText = ImageDraw.Draw(bg_path).text(xy=(65, 75),
+                                         text=cases,
+                                         font=font_type,
+                                         fill="#ffff")
+drawText = ImageDraw.Draw(bg_path).text(xy=(70, 275),
                                          text=deaths,
                                          font=font_type,
                                          fill="#ffff")
-                                            
-deathsBG_resize = deathsBG_path.resize((250, 250), Image.ANTIALIAS)
-deathsBG_file = ImageTk.PhotoImage(deathsBG_resize) 
-deathsBG = Label(root, image=deathsBG_file)
+drawText = ImageDraw.Draw(bg_path).text(xy=(65, 455),
+                                         text=recover,
+                                         font=font_type,
+                                         fill="#ffff")
+bg_file = ImageTk.PhotoImage(bg_path) 
+bg = Label(root, image=bg_file)
 
-#load background recover 
-recoverBG_path = Image.open(osPath + "/assets/img/recover.png")
-drawText = ImageDraw.Draw(recoverBG_path).text(xy=(65, 60),
-                                               text=recover,
-                                               font=font_type,
-                                               fill="#ffff")
-                                            
-recoverBG_resize = recoverBG_path.resize((250, 250), Image.ANTIALIAS)
-recoverBG_file = ImageTk.PhotoImage(recoverBG_resize) 
-recoverBG = Label(root, image=recoverBG_file)
-
-
-#set Grid
-deathsBG.grid(row=0, column = 0)
-recoverBG.grid(row=1, column = 0)
-
-"""
-#fetch data
-for idx, item in enumerate(data):
-    countryLabel = Label(root, text="ประเทศ: " + str(item['country']),font=18)
-    countryLabel.grid(row = idx, column = 0)
-    deathsLabel = Label(root, text="เสียชีวิต: " + str(item['deaths']),font=18)
-    deathsLabel.grid(row = idx, column = 1)
-"""
-
+bg.pack()
 #mainloop
 root.mainloop()
